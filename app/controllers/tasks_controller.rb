750263d676bab1c9269aa32deffd10b157f5c028
toshi32 <viva.la.vida0522@gmail.com>
2021-05-24 18:10:07 +0900

    task コントローラー作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def confirm
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+  def task_params
+  end
+
+  def set_task
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
+  get 'tasks/show'
+  get 'tasks/confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
