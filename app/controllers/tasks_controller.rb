e7d6306b0848aad7e0f9ff0e1a4e6efdbdfd57b4
toshi32 <viva.la.vida0522@gmail.com>
2021-05-24 21:41:14 +0900

    各ページ表示確認及び調整

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.new
+    @tasks = Task.all
   end
 
   def new
